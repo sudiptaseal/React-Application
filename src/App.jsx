0911a92823c7e8b4d0391626a46e93999b957794
sudipta <sudipta_seal08@yahoo.com>
2024-11-28 06:50:03 +0530

    WelcomeMessage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import Header from './components/Header';
-import Main from './components/Main';
-import Footer from './components/Footer';
+import WelcomeMessage from './components/WelcomeMessage';
+
 const App = () => {
   return <div>
-    <Header/>
-    <Main />
-    <Footer/>
+    <WelcomeMessage />
   </div>
 }
 export default App;
