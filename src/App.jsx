7109a49b2a473fc7511cd597217060d1a9f6eff7
sudipta <sudipta_seal08@yahoo.com>
2024-11-29 19:18:56 +0530

    props

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import UserList from './components/UserList';
-import ProductList from './components/ProductList';
+import User from './components/User';
 
 const App = () => {
   return <div>
     
-    <UserList/>
-    <ProductList />
+    <User 
+      img ="https://upload.wikimedia.org/wikipedia/commons/d/dc/Satyajit_Ray_in_New_York_%28cropped%29.jpg"
+      name="Sudipta Seal"
+      age={38}
+      isMarried={false}
+      hobbies={["sleeping","Coding","exercise"]} />
 
   </div>
 }
