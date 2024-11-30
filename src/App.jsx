12153347da28de0bca9941741807251a37322c1c
sudipta <sudipta_seal08@yahoo.com>
2024-11-30 17:36:21 +0530

    props.children

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
-import Person from './components/Person';
-import Product from './components/Product';
+import Card from './components/Card';
 
 const App = () => {
   return <div>
     
-    <Person 
-      name="Sudipta Seal"
-      age={38} />
+    <Card>
+      <h1>Card1</h1>
+      <p>This is card1</p>
+    </Card>
 
-      <Product
-        name="iPhone"
-        price={30000} />
+    <Card>
+      <h1>Card2</h1>
+      <p>This is card2</p>
+    </Card>
+
+    <Card>
+      <h1>Card3</h1>
+      <p>This is card3</p>
+    </Card>
 
   </div>
 }
